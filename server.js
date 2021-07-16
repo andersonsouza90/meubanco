5420c41370feb463e1b814591e47e0343920099b
andersonsouza90 <andersonsilva.souza90@gmail.com>
2021-07-16 00:00:58 -0300

    deploy heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist/meubanco'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/meubanco/index/html');
+  res.sendFile(__dirname + '/dist/meubanco/index.html');
 });
 
 app.listen(PORT, () =>{
